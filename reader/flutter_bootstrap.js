5937a533d4b9f370066573c43d25c82cc417cb4a
Kevin Driedger <linuxbox@extremedesigners.ca>
2026-07-29 07:42:35 -0400

    Deploy Grok Reader web to /reader/ (v1.7.8+137)

--- reader/flutter_bootstrap.js
+++ reader/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=134"},{}]};
+_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=137"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1241397135" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3285726983" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
